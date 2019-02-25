9f91edd00cee589e8aa9ea03ab9366ec11eb0eb3
Daniel Ranchal <danitico98@gmail.com>
2019-02-25 16:50:12 +0100

    hmm

--- P1/src/Practica1/Profesor.java
+++ P1/src/Practica1/Profesor.java
@@ -8,6 +8,25 @@ public class Profesor extends Persona{
     private String universidad_;
     private int id_profesor_;
     
+    public static void main(String[] args){
+        Operaciones operaciones = new Operaciones();
+        Profesor profesor = new Profesor();
+        
+        System.out.println("El factorial de 5 es " + operaciones.Factorial(5));
+        
+        profesor.setNombre("Pedro");
+        profesor.setApellidos("Gómez López");
+        profesor.setEdad(40);
+        profesor.setUniversidad("Universidad de Córdoba");
+        profesor.setId(231655);
+        
+        System.out.println("El profesor " + profesor.getNombre() + " " + 
+                            profesor.getApellidos() + " con edad " + 
+                            profesor.getEdad() + " trabaja en la universidad " + 
+                            profesor.getUniversidad() + " y tiene un id de profesor " + 
+                            profesor.getId() + ".");
+    }
+    
     public String getUniversidad(){
         return universidad_;
     }
